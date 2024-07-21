7cdf7a4c8b8e456fed2c98f58b54cfe39a19ecb9
Hossam Gouda <hos.gouda90@gmail.com>
2024-07-21 17:34:32 +0300

    DS hashing FNV-1a implementation

---
+++ Data-structure/hash.ts
@@ -0,0 +1,34 @@
+class Hash {
+  public Hash32(str: string): number {
+    const OffsetBasis: number = 2166136261;
+    const FNVPrime: number = 16777619;
+    const data: Uint8Array = new TextEncoder().encode(str);
+
+    let hash: number = OffsetBasis;
+    for (let i = 0; i < data.length; i++) {
+      hash ^= data[i];
+      hash *= FNVPrime;
+    }
+    console.log(`${str}, ${hash}, ${hash.toString(16)}`);
+    return hash >>> 0; // Ensure it's treated as a 32-bit unsigned integer
+  }
+
+  public Hash64(str: string): bigint {
+    const OffsetBasis: bigint = BigInt(14695981039346656037);
+    const FNVPrime: bigint = BigInt(1099511628211);
+    const data: Uint8Array = new TextEncoder().encode(str);
+
+    let hash: bigint = OffsetBasis;
+    for (let i = 0; i < data.length; i++) {
+      hash ^= BigInt(data[i]);
+      hash *= FNVPrime;
+    }
+    console.log(`${str}, ${hash}, ${hash.toString(16)}`);
+    return hash;
+  }
+}
+
+// Main program
+const hash = new Hash();
+hash.Hash32("This is Original Text");
+hash.Hash64("This is Original Text");
